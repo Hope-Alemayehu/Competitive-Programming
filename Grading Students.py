eeba87fbcc34a29488d6f5f9008b924b0bf55098
Hope Alemayehu <82286144+Hope-Alemayehu@users.noreply.github.com>
2023-04-21 06:39:48 +0300

    Create Grading Students.py

---
+++ Grading Students.py
@@ -0,0 +1,52 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    
+    for i in range(0,len(grades)):    #loop for all elements in the array
+        
+        if grades[i] < 38:             #comparing the grades
+             continue
+        else:
+             temp = grades[i]                  #finding the difference
+             te = temp % 5
+             if te == 3:                   
+                 temp = temp + 2               #adding 2 for those like 68 ,58..
+                 grades[i] = temp
+             elif te == 4:
+                temp = temp + 1                
+                grades[i] = temp
+             else:
+                continue
+    return grades
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
