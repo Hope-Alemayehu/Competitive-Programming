eb0553d0f0601c758ccc0a5a9d7709360862faa8
Hope Alemayehu <82286144+Hope-Alemayehu@users.noreply.github.com>
2023-05-17 19:30:46 +0300

    Create boat to save people.py

---
+++ boat to save people.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        count=0
+        l,r=0,len(people)-1
+        while l<=r:
+            remain=limit-people[r]
+            r-=1
+            count+=1
+            if l<=r and remain >=people[l]:
+                l+=1
+        return count
