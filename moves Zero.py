0819d0a1b485805b636a93d58c16dc78cbd26a95
Hope Alemayehu <82286144+Hope-Alemayehu@users.noreply.github.com>
2023-05-17 17:06:44 +0300

    Create moves Zero.py

---
+++ moves Zero.py
@@ -0,0 +1,10 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+
+        l=0
+        for i in range(len(nums)):
+            if nums[i]:
+                nums[l],nums[i]=nums[i],nums[l]
+                l+=1
+        return nums
+ 
