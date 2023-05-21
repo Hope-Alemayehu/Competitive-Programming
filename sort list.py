22afa330960aa282546af4aeafebe6103e5138b2
Hope Alemayehu <82286144+Hope-Alemayehu@users.noreply.github.com>
2023-05-21 19:49:08 +0300

    Create sort list.py

---
+++ sort list.py
@@ -0,0 +1,39 @@
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        # Split the list into two halves
+        left = head
+        right = self.getMid(head)
+        temp = right.next
+        right.next = None
+        right = temp
+        left = self.sortList(left)
+        right = self.sortList(right)
+
+        return self.merge(left, right)
+
+    def getMid(self, head):
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+    def merge(self, list1, list2):
+        tail = dummy = ListNode()
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+        if list1:
+            tail.next = list1
+        if list2:
+            tail.next = list2
+
+        return dummy.next
